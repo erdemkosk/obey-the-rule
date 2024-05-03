146ebc319c5f2e1f203930bdcaf30f14a18b50b5
Erdem Köşk <erdemkosk@gmail.com>
2024-05-03 13:19:35 +0300

    Added const on Condition with that we dont have to use before we can work with const

--- src/ICondition.ts
+++ src/ICondition.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Operator } from "./Operator.js";
 
 export interface Condition {
-    fact: string;
+    constant?: any;
+    fact?: string;
     operator: Operator;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     value: any;
   }
--- src/IRule.ts
+++ src/IRule.ts
@@ -2,7 +2,7 @@ import { Action } from "./IAction.js";
 import { Condition } from "./ICondition.js";
 
 export interface Rule {
-    before: Action;
+    before?: Action;
     conditions: {
       and?: Condition[];
       or?: Condition[];
